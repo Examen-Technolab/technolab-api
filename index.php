cf9c1aacfae78c0425d29d646e25634bfdbaa89f
Gerasimova Anastasia <ger.an.a@yandex.ru>
2023-10-13 20:33:47 +0300

    update

--- index.php
+++ index.php
@@ -1,20 +1,20 @@
 <?php
 //проверяем домен и разрешаем запрос
-// $http_origin = $_SERVER['HTTP_ORIGIN'];
+$http_origin = $_SERVER['HTTP_ORIGIN'];
 
-// if ($http_origin == "https://test.examen-technolab.ru" || $http_origin == "https://examen-technolab.ru" || $http_origin == "http://localhost:3000") {
-//     header("Access-Control-Allow-Origin: $http_origin");
-//     header("Access-Control-Allow-Credentials: true");
+if ($http_origin == "https://test.examen-technolab.ru" || $http_origin == "https://examen-technolab.ru" || $http_origin == "http://localhost:3000") {
+    header("Access-Control-Allow-Origin: $http_origin");
+    header("Access-Control-Allow-Credentials: true");
 
-//     // Поддерживаемые методы HTTP
-//     header('Access-Control-Allow-Methods: GET, POST');
+    // Поддерживаемые методы HTTP
+    header('Access-Control-Allow-Methods: GET, POST');
 
-//     // Время, в течение которого веб-браузер может кэшировать ответ (в секундах)
-//     header('Access-Control-Max-Age: 86400');
+    // Время, в течение которого веб-браузер может кэшировать ответ (в секундах)
+    header('Access-Control-Max-Age: 86400');
 
-//     // Список языков, которые веб-браузер может использовать для запросов (разрешаются кросс-доменные запросы)
-//     header('Access-Control-Allow-Headers: Content-Type');
-// }
+    // Список языков, которые веб-браузер может использовать для запросов (разрешаются кросс-доменные запросы)
+    header('Access-Control-Allow-Headers: Content-Type');
+}
 
 
 header('Content-Type: application/json; charset=utf-8');
