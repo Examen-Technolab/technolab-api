5d457ccbd7dc14b1304bff97ab54659c6d444928
Gerasimova Anastasia <ger.an.a@yandex.ru>
2023-10-13 13:17:57 +0300

    fix: sql_file

--- routes/index.php
+++ routes/index.php
@@ -12,7 +12,7 @@ require 'manuals.php';
 function useRouter($connect, $params, $method)
 {
     if ($method == 'GET') {
-        $sql_file = 'sql/' + $params[0] + '.sql';
+        $sql_file = "sql/$params[0].sql";
         getElements($connect, $sql_file);
     }
 
